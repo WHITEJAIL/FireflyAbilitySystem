c32d8cb0fa1b7a317bed05b27d8bedfed2f07ae4
WHITEJAIL <tzlUltraGames@outlook.com>
2022-10-31 01:40:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FireflyAbilitySystem
+ Custom ability system to replace Unreal Engine's GAS
---
+++ Source/FireflyAbilitySystem/Private/FireflyAttributeManager.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireflyAttributeManager.h"
+
+UFireflyAttributeManager::UFireflyAttributeManager(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+float UFireflyAttributeManager::GetAttributeValue(FGameplayTag InAttribute) const
+{
+	float OutValue = 0.f;
+	for (auto Attribute : Attributes)
+	{
+		if (Attribute.GetAttributeName() == InAttribute)
+		{
+			OutValue = Attribute.GetCurrentValue();
+			break;
+		}
+	}
+
+	return OutValue;
+}
+
+float UFireflyAttributeManager::GetAttributeBaseValue(FGameplayTag InAttribute) const
+{
+	float OutBaseValue = 0.f;
+	for (auto Attribute : Attributes)
+	{
+		if (Attribute.GetAttributeName() == InAttribute)
+		{
+			OutBaseValue = Attribute.GetBaseValue();
+			break;
+		}
+	}
+
+	return OutBaseValue;
+}
