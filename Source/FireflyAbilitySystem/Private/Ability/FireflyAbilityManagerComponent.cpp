b2befbd3fadb8cce7dbe09f43f8833d015f87c7b
WHITEJAIL <tzlUltraGames@outlook.com>
2022-11-29 17:25:33 +0800

    三个管理器组件开启复制

--- Source/FireflyAbilitySystem/Private/Ability/FireflyAbilityManagerComponent.cpp
+++ Source/FireflyAbilitySystem/Private/Ability/FireflyAbilityManagerComponent.cpp
@@ -13,7 +13,7 @@ UFireflyAbilityManagerComponent::UFireflyAbilityManagerComponent(const FObjectIn
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	SetIsReplicated(true);
+	SetIsReplicatedByDefault(true);
 
 	// ...
 }
